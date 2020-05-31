7d0141f892633c7356f4b5e824f803b5906bc2ca
ndo@ndo3 <yo@ndo.dev>
2020-05-31 10:34:11 +0200

    db querying itenos per sheet!

--- app/components/upload.tsx
+++ app/components/upload.tsx
@@ -1,7 +1,10 @@
 import React, { useMemo, useState, useCallback } from 'react'
 import { useDropzone } from 'react-dropzone'
 import styles from './upload.css'
-import db from '../utils/db'
+import q from '../utils/db'
+
+const fs = require('fs')
+const ExcelJS = require('exceljs')
 
 const baseStyle = {
   flex: 1,
@@ -47,8 +50,8 @@ type File = {
 
 const Upload = () => {
   const [myFiles, setMyFiles] = useState<File>([])
+  const [working, setWorking] = useState(false)
   const onDrop = useCallback(acceptedFiles => {
-    console.log(acceptedFiles)
     setMyFiles([...myFiles, ...acceptedFiles])
     // acceptedFiles.forEach(file => {
     //   const reader = new FileReader()
@@ -73,17 +76,62 @@ const Upload = () => {
   //   setMyFiles([]);
   // };
 
-  const handleUpload = data => {
-    db()
-    console.log(data)
+  const handleCompare = async (path: string, type: string) => {
+    setWorking(true)
+    const workbook = new ExcelJS.Workbook()
+    const data = await fs.promises.readFile(path)
+    const wb = await workbook.xlsx.load(data.buffer)
+    if (type === 'itenos') {
+      const orders = {}
+      wb.eachSheet((worksheet, sheetId: number) => {
+        console.log(sheetId)
+        orders[sheetId] = []
+        worksheet.eachRow((row, rowNumber) => {
+          // console.log('Row ' + rowNumber + ' = ' + JSON.stringify(row.values));
+          if (typeof row.values[1] === 'number') {
+            orders[sheetId].push({
+              order: row.values[2],
+              po: row.values[3],
+              mrc: row.values[5],
+            })
+          }
+        })
+      })
+      console.log(orders)
+      Object.values(orders).forEach(sheet => {
+        const orderQuery = sheet.map(row => {
+          return row.order
+        })
+        if (orderQuery.length) {
+          q(`select distinct Nummer, SummeNetto 
+            from BestKopf2 
+            where Nummer in (${orderQuery.join(', ')})`)
+            .then(resp => {
+              console.log(resp)
+              return resp
+            })
+            .catch(err => console.error(err))
+        }
+      })
+
+      setWorking(false)
+    }
+    // const newworksheet = newWorkbook.getWorksheet('My Sheet')
+    // newworksheet.columns = [
+    //   { header: 'Id', key: 'id', width: 10 },
+    //   { header: 'Name', key: 'name', width: 32 },
+    //   { header: 'D.O.B.', key: 'dob', width: 15 },
+    // ]
+    // await newworksheet.addRow({
+    //   id: 3,
+    //   name: 'New Guy',
+    //   dob: new Date(2000, 1, 1),
+    // })
+
+    // await newWorkbook.xlsx.writeFile('export2.xlsx')
   }
 
   const onDropRejected = useCallback(data => {
-    console.log(data)
-    // const img = nativeImage.createFromPath(
-    //   '../assets/img/icons/nt-512-grey.png'
-    // )
-    // console.log(img)
     if (data[0].errors[0].code === 'file-invalid-type') {
       new Notification('Newtelco', {
         body: 'Only Excel files allowed',
@@ -114,14 +162,14 @@ const Upload = () => {
         <button
           type='button'
           className={styles.fileButton}
-          onClick={() => handleUpload(file.path)}
+          onClick={() => handleCompare(file.path, 'itenos')}
         >
           Itenos
         </button>
         <button
           type='button'
           className={styles.fileButton}
-          onClick={() => handleUpload(file.path)}
+          onClick={() => handleCompare(file.path, 'equinix')}
         >
           Equinix
         </button>
@@ -146,29 +194,50 @@ const Upload = () => {
     [isDragActive, isDragReject, isDragAccept]
   )
 
-  return (
-    <>
-      <div className={styles.dragWrapper} {...getRootProps({ style })}>
-        <input {...getInputProps()} />
-        {isDragActive ? (
-          <p>Drop the files here ...</p>
-        ) : (
-          <p>
-            <i className='far fa-file-excel' style={{ marginRight: '10px' }} />
-            Drop your Excel File Here
-          </p>
-        )}
+  if (!working) {
+    return (
+      <>
+        <div className={styles.dragWrapper} {...getRootProps({ style })}>
+          <input {...getInputProps()} />
+          {isDragActive ? (
+            <p>Drop the files here ...</p>
+          ) : (
+            <p>
+              <i
+                className='far fa-file-excel'
+                style={{ marginRight: '10px' }}
+              />
+              Drop your Excel File Here
+            </p>
+          )}
+        </div>
+        <ul className={styles.fileList}>{files}</ul>
+        <div>
+          {!myFiles.length && (
+            <>
+              <div className={styles.noteWrapper}>
+                Only `.xls` or `.xslx` files allowed
+              </div>
+            </>
+          )}
+        </div>
+      </>
+    )
+  } else {
+    return (
+      <div className='lds-grid'>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
       </div>
-      <ul className={styles.fileList}>{files}</ul>
-      {!myFiles.length && (
-        <>
-          <div className={styles.noteWrapper}>
-            Only `.xls` or `.xslx` files allowed
-          </div>
-        </>
-      )}
-    </>
-  )
+    )
+  }
 }
 
 export default Upload
---
+++ app/utils/db.tsx
@@ -0,0 +1,49 @@
+const mssql = require('mssql')
+
+// const db = async () => {
+//   // let connection
+//   // make sure that any items are correctly URL encoded in the connection string
+//   console.log(mssql)
+//   console.log(process.env.MSSQL_USERNAME)
+//   const config = {
+//     user: process.env.MSSQL_USERNAME,
+//     password: process.env.MSSQL_PW,
+//     server: process.env.MSSQL_SERVER,
+//     database: process.env.MSSQL_DB,
+//   }
+//   const connection = await mssql.connect(config)
+//   // const result = await mssql.query`select I3D, Freitext from AbholPos`
+//   // console.dir(result)
+//   return connection
+//   // return connection
+// }
+
+const config = {
+  user: process.env.MSSQL_USERNAME,
+  password: process.env.MSSQL_PW,
+  server: process.env.MSSQL_SERVER,
+  database: process.env.MSSQL_DB,
+}
+
+const db = mssql.connect(config)
+
+const q = async query => {
+  try {
+    const results = await mssql.query(query)
+    return results
+  } catch (error) {
+    return { error }
+  }
+}
+export default q
+
+// exports.query = async query => {
+
+//   try {
+//     const results = await db.query(query)
+//     await db.end()
+//     return results
+//   } catch (error) {
+//     return { error }
+//   }
+// }
