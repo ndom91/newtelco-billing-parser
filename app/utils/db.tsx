ea3ec9b66b3130dbdba61a5b897fc022d5102254
ndo@ndo3 <yo@ndo.dev>
2020-06-01 12:14:41 +0200

    add: more logging!

--- app/utils/db.tsx
+++ app/utils/db.tsx
@@ -2,6 +2,8 @@ const mssql = require('mssql')
 const path = require('path')
 const log = require('electron-log')
 
+log.catchErrors()
+
 const config = {
   user: process.env.MSSQL_USERNAME,
   password: process.env.MSSQL_PW,
@@ -10,10 +12,13 @@ const config = {
 }
 
 mssql.connect(config)
+log.info(mssql)
 
 const q = async (query: string) => {
+  log.info(query)
   try {
     const results = await mssql.query(query)
+    log.info(results)
     return results
   } catch (error) {
     log.error(error)
